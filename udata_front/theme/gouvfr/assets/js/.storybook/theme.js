b24dfdd20c79952d1febd8ff31095173cb0f60b6
Adriano Leal <adriano.leal@babelgroup.com>
2024-10-15 17:43:50 +0100

    Add orange theme

--- udata_front/theme/gouvfr/assets/js/.storybook/theme.js
+++ udata_front/theme/gouvfr/assets/js/.storybook/theme.js
@@ -1,15 +1,16 @@
-import { create } from '@storybook/theming/create';
-import logo from '../../../templates/svg/logotype.svg';
+import { create } from "@storybook/theming/create";
+import logo from "../../../templates/svg/logotype.svg";
 
 export default create({
-  base: 'light',
+  base: "light",
   // Typography
-  fontBase: 'Marianne, sans-serif',
-  fontCode: 'monospace',
+  fontBase: "Marianne, sans-serif",
+  fontCode: "monospace",
 
-  brandTitle: 'data.gouv.fr Storybook',
-  brandUrl: 'https://www.data.gouv.fr',
+  brandTitle: "data.gouv.fr Storybook",
+  brandUrl: "https://www.data.gouv.fr",
   brandImage: logo,
-  colorPrimary: '#161616',
-  colorSecondary: '#3558a2'
+  colorPrimary: "#161616",
+  // colorSecondary: "#3558a2",
+  colorSecondary: "#e4794a",
 });
