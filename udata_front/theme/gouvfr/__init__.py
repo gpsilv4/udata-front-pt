53dc0c0877a3647199170a7c9253e6a717258fdb
adbrum <adbrum@outlook.com>
2024-10-30 17:50:15 +0000

    Add About us on footer

--- udata_front/theme/gouvfr/__init__.py
+++ udata_front/theme/gouvfr/__init__.py
@@ -130,7 +130,8 @@ nav.Bar("gouvfr_opendata", opendata_links)
 
 
 platform_links = [
-    nav.Item(_("Guides"), None, url=current_app.config.get("GUIDES_URL", "#")),
+    nav.Item(_("About us"), "gouvfr.show_page", args={"slug": "faqs/about_dadosgov"}),
+    # nav.Item(_("Guides"), None, url=current_app.config.get("GUIDES_URL", "#")),
     nav.Item(_("API Tutorial"), "gouvfr.show_page", args={"slug": "api-tutorial"}),
     nav.Item(_("Roadmap and news"), "gouvfr.show_page", args={"slug": "roadmap"}),
     nav.Item(_("Contact us"), "gouvfr_faq.contact", args={"slug": "contact"}),
