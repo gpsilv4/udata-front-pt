da8e850bd6007fa794a0bbae9966d407e4578fc5
Gonçalo <goncalo_silv@hotmail.com>
2024-11-19 12:50:40 +0000

    udata-front 6.0.0 version update

--- udata_front/__init__.py
+++ udata_front/__init__.py
@@ -1,9 +1,9 @@
 '''
-udata customizations for data.gouv.fr
+udata customizations for dados.gov
 '''
 
-__version__ = '5.2.4'
-__description__ = 'udata customizations for data.gouv.fr'
+__version__ = '6.0.0'
+__description__ = 'udata customizations for dados.gov'
 
 APIGOUVFR_EXTRAS_KEY = 'apigouvfr:apis'
 APIGOUVFR_EXPECTED_FIELDS = [
