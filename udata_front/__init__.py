d5f33e972638df5877203b8f2a3164fa30b03a54
Gonçalo <goncalo_silv@hotmail.com>
2024-11-21 10:27:31 +0000

    1ª part of the update 6.0.3

--- udata_front/__init__.py
+++ udata_front/__init__.py
@@ -2,16 +2,5 @@
 udata customizations for dados.gov
 '''
 
-__version__ = '6.0.0'
+__version__ = '6.0.3'
 __description__ = 'udata customizations for dados.gov'
-
-APIGOUVFR_EXTRAS_KEY = 'apigouvfr:apis'
-APIGOUVFR_EXPECTED_FIELDS = [
-    'title',
-    'tagline',
-    'path',
-    'slug',
-    'owner',
-    'openness',
-    'logo',
-]
--- udata_front/theme/gouvfr/__init__.py
+++ udata_front/theme/gouvfr/__init__.py
@@ -46,6 +46,7 @@ gouvfr_menu = nav.Bar(
     "gouvfr_menu",
     [
         nav.Item(_("Data"), "datasets.list"),
+        nav.Item(_('API'), 'dataservices.list'),
         nav.Item(_("Reuses"), "reuses.list"),
         nav.Item(_("Organizations"), "organizations.list"),
         nav.Item(
